1d816b2af3e55528b6b2a68d98d91d3c6ff2de4b
NickArancibia <narancibiacarabajal@gmail.com>
2024-05-15 20:06:56 -0300

    Add IDT

---
+++ Kernel/irqDispatcher.c
@@ -0,0 +1,18 @@
+#include <time.h>
+#include <stdint.h>
+#include <naiveConsole.h>
+
+static void int_20();
+
+void irqDispatcher(uint64_t irq) {
+	switch (irq) {
+		case 0:
+			int_20();
+			break;
+	}
+	return;
+}
+
+void int_20() {
+	timer_handler();
+}
