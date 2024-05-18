76c916b56451380ad6ff03c4350375e80510bb36
NickArancibia <narancibiacarabajal@gmail.com>
2024-05-18 00:08:48 -0300

    Fix function name

--- Kernel/syscallDispatcher.c
+++ Kernel/syscallDispatcher.c
@@ -3,12 +3,12 @@
 #include <videoDriver.h>
 
 static uint64_t(*syscallHandlers[])(uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t r10, uint64_t r8)={
-    
+
 };         // Handlers for syscalls, e.g. read, write, etc
 
 int handlersSize=sizeof(syscallHandlers)/sizeof(syscallHandlers[0]);
 
-uint64_t syscallHandler(uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t r10, uint64_t r8, uint64_t rax){         
+uint64_t syscallDispatcher(uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t r10, uint64_t r8, uint64_t rax){         
     if(rax < handlersSize && syscallHandlers[rax]!=NULL)                                                            // Compares with NULL in case it is not implemented yet
         return syscallHandlers[rax](rdi,rsi,rdx,r10,r8);
     return -1;
