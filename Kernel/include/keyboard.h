32b876fba05ecf84f12ad047c95cf675753f93b4
Bordita <bordatomas1@gmail.com>
2024-05-18 14:05:20 -0300

    keyboardDriver update but not complete

--- Kernel/include/keyboard.h
+++ Kernel/include/keyboard.h
@@ -6,38 +6,36 @@
 void updateBuffer(void);
 void cleanBuffer(void);
 uint8_t getKey(void);
+char readLastCharacter(void);
 
 
-
-static unsigned char scan_code_chars[2][] = {
+static unsigned char scanCodes[2][63] = {
     {
     0,'\x1B', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',   
     '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',    
     '\x11', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',       
     '\x12', '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '\x14',   
-    '\x10', ' ', '\x3A', '\x80', '\x81', '\x82', '\x83', '\x84', '\x85', '\x86', '\x87', '\x88', '\x89'
-    },
-    {
-    0,'\x1B', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '\b',    
+    '\x10', ' ', '\x3A', '\x80', '\x81', '\x82', '\x83', '\x84', '\x85', '\x86', '\x87', '\x88', '\x89'},
+    {0,'\x1B', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '\b',    
     '\t', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n',    
     '\x11', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', '~',        
     '\x12', '|', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '\x14',      
-    '\x10', ' ', '\x3A', '\x80', '\x81', '\x82', '\x83', '\x84', '\x85', '\x86', '\x87', '\x88', '\x89'  
-    }  
+    '\x10', ' ', '\x3A', '\x80', '\x81', '\x82', '\x83', '\x84', '\x85', '\x86', '\x87', '\x88', '\x89'}  
 };
 
-enum CONTROLCHARS {
- ESCAPE            = '\x1B' 
- BACKSPACE         = '\b'
- TAB               = '\t' 
- ENTER             = '\n' 
- CONTROL           = '\x11'
- L_SHIFT_PRESS     = '\x12' 
- L_SHIFT_RELEASE   = '\xAA' 
- R_SHIFT_PRESS     = '\x14'
- R_SHIFT_RELEASE   = '\xB6' 
- L_ALT             = '\x10' 
- CAPSLOCK          = '\x3A' 
-}
+enum SPECIALCHARS {
+ ESCAPE            = '\x1B', 
+ BACKSPACE         = '\b',
+ TAB               = '\t', 
+ ENTER             = '\n', 
+ CONTROL           = '\x11',
+ L_SHIFT_PRESS     = '\x12', 
+ L_SHIFT_RELEASE   = '\xAA', 
+ R_SHIFT_PRESS     = '\x14',
+ R_SHIFT_RELEASE   = '\xB6', 
+ L_ALT             = '\x10', 
+ CAPSLOCK          = '\x3A', 
+ SPACE_BAR         = ' '
+};
 
 #endif
--- Kernel/keyboardDriver.c
+++ Kernel/keyboardDriver.c
@@ -5,15 +5,77 @@
 
 static unsigned char buffer[MAXSIZE]={0};
 static int bufferPos=0;                     // Indicates your current position in buffer
+static int shift = 0;
+
+
+int isSpecialKey(char key);
+
+
+int isSpecialKey(char key){
+    switch(key) {
+        case ESCAPE:
+            return 1;
+
+        case L_SHIFT_PRESS:
+        case R_SHIFT_PRESS:
+            shift = !shift;
+            return 1; 
+
+        case L_SHIFT_RELEASE:
+        case R_SHIFT_RELEASE:
+            shift = !shift;
+            return 1;
+
+        case SPACE_BAR:
+            //handle mov
+            bufferPos++;
+            return 1;
+
+        case TAB:
+            //handle mov
+            bufferPos++;
+            return 1;
+
+        case ENTER:
+            buffer[bufferPos] = 0;
+            return 1;
+    
+        case CAPSLOCK:
+            shift = !shift;
+            return 1;        
+        
+        case BACKSPACE:
+            // handle delete
+            bufferPos--;
+            return 1;
+        default:
+            return 0; 
+    }
+}
 
 void updateBuffer(){
-    uint8_t key = getKey();
+    uint8_t scancode = getKey();
     
-    buffer[bufferPos]=scanCodes[key];
+    char c = scanCodes[shift][scancode];
+    
+    if(!isSpecialKey(c) == 1){  
+    buffer[bufferPos]=c;
     ncPrintChar(buffer[bufferPos]);
     bufferPos++;
+    if(bufferPos >= MAXSIZE)
+        bufferPos = 0;
+    }
 }
 
 void cleanBuffer(){
     bufferPos=0;
 }
+
+char readLastCharacter(){
+    if(bufferPos > 1){
+        return buffer[bufferPos-1];
+    }
+    return 0;
+}
+
+
