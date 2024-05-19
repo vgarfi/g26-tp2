4b9816a772253ece2ee8dcff3268297e7ccdb85f
Nicole <nsalama@itba.edu.ar>
2024-05-19 11:38:13 -0300

    added test to getKey

--- Kernel/asm/keyboard.asm
+++ Kernel/asm/keyboard.asm
@@ -5,7 +5,13 @@ getKey:
     cli
 
     xor rax, rax
-    in al, 60h
+    in al, 64h      ;leo el teclado
 
+    test al, 0x1    ; comparo con el bit 1 para ver si hay una letra para leer
+    je getKey
+
+    ;hay una letra para leer
+    in al, 60h      ; donde deja la tecla se leyo (lo deja en dormato scancode)
+    
     sti
     ret
