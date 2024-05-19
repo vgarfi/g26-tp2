587927fb95ba4fcab0bd5d458e82ef53e4e4e0c6
Bordita <bordatomas1@gmail.com>
2024-05-19 00:56:24 -0300

    working except for 16bits per row

--- Kernel/videoDriver.c
+++ Kernel/videoDriver.c
@@ -49,22 +49,52 @@ typedef struct vbe_mode_info_structure * VBEInfoPtr;
 
 VBEInfoPtr VBE_mode_info = (VBEInfoPtr) 0x0000000000005C00;
 
-
-void putPixel(uint32_t hexColor, uint64_t x, uint64_t y) {
-    uint8_t * framebuffer = (uint8_t *) VBE_mode_info->framebuffer;
-    uint64_t offset = (x * ((VBE_mode_info->bpp)/8)) + (y * VBE_mode_info->pitch);
-    framebuffer[offset]     =  (hexColor) & 0xFF;
+void putPixel(uint8_t* framebuffer,uint64_t offset,uint32_t hexColor){
+	framebuffer[offset]     =  (hexColor) & 0xFF;
     framebuffer[offset+1]   =  (hexColor >> BYTE_LENGHT) & 0xFF; 
     framebuffer[offset+2]   =  (hexColor >> TWO_BYTE_LENGHT) & 0xFF;
 }
+void putPixelAt(uint32_t hexColor, int x, int y) {
+    uint8_t * framebuffer = (uint8_t *) VBE_mode_info->framebuffer;
+    uint64_t offset = (x * ((VBE_mode_info->bpp)/8)) + (y * VBE_mode_info->pitch);
+    putPixel(framebuffer,offset,hexColor);
+}
 
+void printLine(uint8_t* framebuffer,uint64_t offset,uint32_t fgColor,uint32_t bgColor ,uint8_t mask,int limit,int step){
+	uint64_t off = offset;
+	uint32_t hexColor;
+	for(int i=0; i < limit; i++,off+=step){
+		if (mask & (1 << (BYTE_LENGHT - 1 - i))) {
+			hexColor = fgColor;
+		} 
+		else{
+			hexColor = bgColor;
+		}
+		putPixel(framebuffer,off,hexColor);
+	}
+}
+
+void printLineAt(int x,int y,uint32_t fgColor,uint32_t bgColor, uint8_t mask,int limit,int step){
+	uint8_t * framebuffer = (uint8_t *) VBE_mode_info->framebuffer;
+	uint64_t offset = (x * ((VBE_mode_info->bpp)/8)) + (y * VBE_mode_info->pitch);
+	printLine(framebuffer,offset,fgColor,bgColor,mask,limit,step);
+}
+
+void printRectAt(int x1,int y1,int x2,int y2,uint32_t hexColor){
+	int limit = x2 - x1;
+	uint8_t * framebuffer = (uint8_t *) VBE_mode_info->framebuffer;
+	uint64_t offset = (x1 * ((VBE_mode_info->bpp)/8)) + (y1 * VBE_mode_info->pitch);
+	for(;y1<y2;y1++,offset+=VBE_mode_info->pitch){
+		printLine(framebuffer,offset,hexColor,hexColor,255,limit,((VBE_mode_info->bpp)/8));
+	}
+}
 void print_char_row(int x_offset, int y, unsigned char data) {
   for (int x = 0; x < BYTE_LENGHT; x++) {
     // Check if the corresponding bit is set (1)
     if (data & (1 << (BYTE_LENGHT - 1 - x))) {
-      putPixel(0x00ffffff,x_offset + x, y); // foreground
+      putPixelAt(0x00ffffff,x_offset + x, y); // foreground
     } else {
-      putPixel(0x00000000,x_offset + x, y); // background
+      putPixelAt(0x00000000,x_offset + x, y); // background
 	}
   }
 }
@@ -75,9 +105,9 @@ void print_char_row_2_byte(int x_offset, int y, unsigned char data1, unsigned ch
     int bit = x < BYTE_LENGHT ? x : x - BYTE_LENGHT;
 
     if (data & (1 << (BYTE_LENGHT - 1 - bit))) {
-      putPixel(0x00ffffff,x_offset + x, y); // foreground
+      putPixelAt(0x00ffffff,x_offset + x, y); // foreground
     } else {
-      putPixel(0x00000000,x_offset + x, y); // background
+      putPixelAt(0x00000000,x_offset + x, y); // background
     }
   }
 }
@@ -88,13 +118,19 @@ void print_char(int x, int y, unsigned char c) {
 	int width = fontBitMap.size.width;
 	int height = fontBitMap.size.height;
 
+	uint8_t * framebuffer = (uint8_t *) VBE_mode_info->framebuffer;
+	uint64_t offset = (x * ((VBE_mode_info->bpp)/8)) + (y * VBE_mode_info->pitch);
+
 	if (width == BYTE_LENGHT) {
-		for (int y_pos = 0; y_pos < height; y_pos++) {
-			print_char_row(x, y + y_pos, bitmap[y_pos + (c-31) * height]);
+		for (int y_pos = 0; y_pos < height; y_pos++,offset+=VBE_mode_info->pitch) {
+			printLine(framebuffer,offset,0x00ffffff,0x00000000,bitmap[y_pos + (c-31) * height],BYTE_LENGHT,((VBE_mode_info->bpp)/8));
+			//print_char_row(x, y + y_pos, bitmap[y_pos + (c-31) * height]);
 		}
 	}
 	else {
-		for (int y_pos = 0, pY = 0; y_pos < height*2; y_pos+=2, pY++) {
+		for (int y_pos = 0, pY = 0; y_pos < height*2; y_pos+=2, pY++,offset+=VBE_mode_info->pitch) {
+		//	printLine(framebuffer,offset,0x00FFFFFF,0x00000000,bitmap[y_pos + (c-31) * height*2],BYTE_LENGHT,((VBE_mode_info->bpp)/8));
+		//	printLine(framebuffer,offset+(width*((VBE_mode_info->bpp)/8)),0x00ffffff,0x00000000,bitmap[y_pos + (c-31) * height*2 + 1],BYTE_LENGHT,((VBE_mode_info->bpp)/8));
 			unsigned char data1 = bitmap[y_pos + (c-31) * global_font_manager.fonts[global_font_manager.currentFontIndex].size.height*2];
 			unsigned char data2 = bitmap[y_pos + (c-31) * global_font_manager.fonts[global_font_manager.currentFontIndex].size.height*2 + 1];
 			print_char_row_2_byte(x, y + pY, data1, data2);
