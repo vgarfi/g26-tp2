1f0f38e6dcdd75d2fe2086cfd641cc0f364ef488
Nicole <nsalama@itba.edu.ar>
2024-05-19 23:34:41 -0300

    time.asm simplified

--- Kernel/asm/time.asm
+++ Kernel/asm/time.asm
@@ -6,91 +6,67 @@ GLOBAL rtc_get_month
 GLOBAL rtc_get_year
 
 section .text
-rtc_get_seconds:
+
+%macro rtc_get 1
     cli
 
-    mov al, 0x0B
-    out 70h, al
-    in al, 71h
-    or al, 4
-    out 71h, al     ; Sets Seconds to be returned in binary
+    ; Argumento: registro a leer (en AL)
+    ; Lee el registro del RTC y devuelve el valor leído en AL
+    ; Selecciona el registro a leer
 
-    mov al, 0x00
+; hago un loop hasta que haya data available, eso esta en el registro 0A
+; en el bit 7 (por eso hago test al, 8) se compara el octavo bit si hay 
+; data available se prende el bit y sino se apaga
+
+    ;loop:
+    ;    mov al, 0xA
+    ;    out 0x70, al
+    ;    in al, 0x71
+    ;    test al, 0x80
+    ;    jne loop
+
+    xor rax, rax
+    mov al, 0xB
+    out 0x70, al 
+
+    ; Lee el valor del registro
+    in al, 0x71
+    or al, 4
+    out 0x71, al    ; Sets Seconds to be returned in binary
+
+    mov al, %1      ; Argumento (registro a leer)
     out 70h, al
     in al, 71h
 
     sti
+%endmacro
+
+rtc_get_seconds:
+    rtc_get 0x00
 
     ret
 
 rtc_get_minutes:
-    cli
+    rtc_get 0x02
 
-    mov al, 0x0B
-    out 70h, al
-    in al, 71h
-    or al, 4
-    out 71h, al     ; Sets Minutes to be returned in binary
-
-    mov al, 0x02
-    out 70h, al
-    in al, 71h
-
-    sti
     ret
 
 rtc_get_hour:
-    mov al, 0x0B
-    out 70h, al
-    in al, 71h
-    or al, 4
-    out 71h, al     ; Sets Hours to be returned in binary
+    rtc_get 0x04
 
-    mov al, 0x04
-    out 70h, al
-    in al, 71h
-
-    sti
     ret
 
-rtc_get_day:        ;; Returns day of the month
-    mov al, 0x0B
-    out 70h, al
-    in al, 71h
-    or al, 4
-    out 71h, al     ; Sets Day to be returned in binary
+rtc_get_day:        ; Returns day of the month
+    rtc_get 0x07
 
-    mov al, 0x07
-    out 70h, al
-    in al, 71h
-
-    sti
     ret
 
 rtc_get_month:
-    mov al, 0x0B
-    out 70h, al
-    in al, 71h
-    or al, 4
-    out 71h, al     ; Sets Month to be returned in binary
+    rtc_get 0x08
 
-    mov al, 0x08
-    out 70h, al
-    in al, 71h
-
-    sti
     ret
 
 rtc_get_year:
-    mov al, 0x0B
-    out 70h, al
-    in al, 71h
-    or al, 4
-    out 71h, al     ; Sets Year to be returned in binary
+    rtc_get 0x09
 
-    mov al, 0x09
-    out 70h, al
-    in al, 71h
-
-    sti
     ret
