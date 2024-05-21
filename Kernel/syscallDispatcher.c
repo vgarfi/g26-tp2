ff854cf311c3eb72de11dbd62ff5ff82f66dfd79
NickArancibia <narancibiacarabajal@gmail.com>
2024-05-20 23:12:53 -0300

    add buffer in dispatcher

--- Kernel/syscallDispatcher.c
+++ Kernel/syscallDispatcher.c
@@ -4,11 +4,15 @@
 #include <time.h>
 
 int nanosleep(uint64_t rdi);     // rdi : seconds, rsi : miliseconds
-int saveregs(uint64_t rdi);
+int saveregs(void);
+
+void saveRegsInBuffer(uint64_t* buf);
+
+uint64_t registers[17]={0};
 
 uint64_t syscallDispatcher(uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t r10, uint64_t r8, uint64_t rax){         
     switch(rax){
-        case 3: return saveregs(rdi);
+        case 3: return saveregs();
         case 162: return nanosleep(rdi);
         default: return -1;
     }
@@ -21,10 +25,10 @@ int nanosleep(uint64_t rdi){
     return 0;
 }
 
-/** rdi stores a buffer direction of 17 qwords to save registers in the next order:
+/** registers is a buffer of 17 qwords to save registers in the next order:
  *  RAX RBX RCX RDX RSI RDI RBP RSP R8 R9 R10 R11 R12 R13 R14 R15 RIP
  */
-int saveregs(uint64_t rdi){
-    saveRegsInBuffer(rdi);
+int saveregs(){
+    saveRegsInBuffer(registers);
     return 0;
 }
