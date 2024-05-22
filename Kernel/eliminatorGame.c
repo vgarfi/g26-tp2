16aff0d4dafce8ae8211657b723ca5b63fd8a384
vgarfi <valentin.garfi@gmail.com>
2024-05-22 16:58:11 -0300

    feat: started eliminator ft Nicole

--- Kernel/eliminatorGame.c
+++ Kernel/eliminatorGame.c
@@ -1,6 +1,27 @@
 #include "eliminatorGame.h"
 #include "videoDriver.h"
 
+#define RED 0x00FF0000
+#define GREEN 0x0000FF00
+#define BLUE 0x000000FF
+
+#define PROV_WIDTH  1000
+#define PROV_HEIGHT 600
+
 void eliminatorGame () {
-    vdPrintRect(0x00FF0000);
+    static char board [PROV_WIDTH][PROV_HEIGHT] = {0};
+    printWall();
+}
+
+void printWall () {
+    for (int i = PROV_WIDTH/2, k = PROV_WIDTH/2; i >= 0 && k < PROV_WIDTH ; i--, k++) {
+        vdSetCursor(i, 0);
+        vdPrintRect(RED);
+        vdSetCursor(k, 0);
+        vdPrintRect(RED);
+        for (int i = 0; i < 900000; i++)
+        {
+        }
+        
+    }   
 }
