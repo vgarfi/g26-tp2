6a3bb1dd716856a317b94393030b5aede9b93d7d
Bordita <bordatomas1@gmail.com>
2024-05-31 23:14:15 -0300

    videoDriver refactored

--- Kernel/videoDriver.c
+++ Kernel/videoDriver.c
@@ -3,36 +3,50 @@
 #include <fonts.h>
 #include <lib.h>
 
-#define MAXCHARSINSCREEN 10880	// chars per row * chars per column 
+#define MAXCHARSINSCREEN 10880	// chars per row * chars per column (with minimum size)
+#define GREY 0x00F0F0F0
+#define BLACK 0x00000000
 
 typedef struct vbe_mode_info_structure * VBEInfoPtr;
 VBEInfoPtr VBE_mode_info = (VBEInfoPtr) 0x0000000000005C00;
-Cursor cursor = {0, 0,0x00F0F0F0};
+Cursor cursor = {0, 0,GREY};
 uint8_t * framebuffer;
 uint16_t widthScreen;
 uint16_t heightScreen;
 uint16_t pitch;
 uint8_t  bytesPerPixel;
-static uint32_t fgColor = 0x00F0F0F0;
-static uint32_t bgColor = 0x00000000;
-static int maxCharsInScreen = 10880; // chars per row * chars per column 
-char charsInScreen[10880] = {' '};
-uint32_t colorsInScreen[10880];
-static int index = 0;
-static int scrollUpdateBuffer = 1;
+static uint32_t fgColor = GREY;
+static uint32_t bgColor = BLACK;
+char charsInScreen[MAXCHARSINSCREEN];
+uint32_t colorsInScreen[MAXCHARSINSCREEN];
+static int index;
+static int scrollUpdateBuffer;
+
 void initializeVideoDriver(){
 	framebuffer = (uint8_t *) VBE_mode_info->framebuffer;
 	widthScreen = 	VBE_mode_info->width;
 	heightScreen = VBE_mode_info->height;
 	pitch = VBE_mode_info->pitch;
 	bytesPerPixel = VBE_mode_info->bpp/BYTE_LENGHT;
-	//memset(charsInScreen,)
-	memset(charsInScreen,' ',maxCharsInScreen);
-	memset(colorsInScreen,0,4*maxCharsInScreen);
+	memset(charsInScreen,' ',MAXCHARSINSCREEN);
+	memset(colorsInScreen,0,4*MAXCHARSINSCREEN);
 	scrollUpdateBuffer = 1;
-
+	index = 0;
 }
 
+void vdPrintLine(uint64_t offset,uint32_t fgColor,uint32_t bgColor ,uint8_t mask,int limit,int step){
+	uint64_t off = offset;
+	uint32_t hexColor;
+	for(int i=0; i < limit; i++,off+=step){
+		if (mask & (1 << (BYTE_LENGHT - 1 - i))) {
+			hexColor = fgColor;
+		} 
+		else{
+			hexColor = bgColor;
+		}
+		vdPutPixel(off,hexColor);
+	}
+}
 
 void vdSetCursorColor(uint32_t hexcolor){
 	cursor.color = hexcolor;
@@ -103,7 +117,7 @@ void vdSetCursor(int x, int y){
 }
 
 void vdNewLine(){
-	vdPrintRect(cursor.posX/bytesPerPixel,cursor.posY/pitch,getCurrentFont().size.realWidth,getCurrentFont().size.height,0x00000000);
+	vdPrintRect(cursor.posX/bytesPerPixel,cursor.posY/pitch,getCurrentFont().size.realWidth,getCurrentFont().size.height,BLACK);
 	cursor.posX = 0;
 	vdUpdateCursor(0,1);
 	vdPrintCursor();
@@ -113,20 +127,6 @@ void vdPutPixel(uint64_t offset,uint32_t hexColor){
 	framebuffer[offset]     =  (hexColor) & 0xFF;
     framebuffer[offset+1]   =  (hexColor >> BYTE_LENGHT) & 0xFF; 
     framebuffer[offset+2]   =  (hexColor >> TWO_BYTE_LENGHT) & 0xFF;
-}
-
-void vdPrintLine(uint64_t offset,uint32_t fgColor,uint32_t bgColor ,uint8_t mask,int limit,int step){
-	uint64_t off = offset;
-	uint32_t hexColor;
-	for(int i=0; i < limit; i++,off+=step){
-		if (mask & (1 << (BYTE_LENGHT - 1 - i))) {
-			hexColor = fgColor;
-		} 
-		else{
-			hexColor = bgColor;
-		}
-		vdPutPixel(off,hexColor);
-	}
 }
 
 void vdPrintChar(unsigned char c) {
@@ -159,7 +159,7 @@ void vdPrint(char *characters,uint32_t hexColor){
 		char c = characters[i];
 
 		if(c == '\n'){
-			colorsInScreen[index] = 0x00000000;
+			colorsInScreen[index] = BLACK;
 			charsInScreen[index++] = '\n';
 			vdNewLine();
 		}
@@ -168,7 +168,7 @@ void vdPrint(char *characters,uint32_t hexColor){
 			vdDeleteChar();
 			if(index != 0)
 				--index;
-			colorsInScreen[index] = 0x00000000;
+			colorsInScreen[index] = BLACK;
 			charsInScreen[index] = ' ';
 		}
 		else{
@@ -180,46 +180,25 @@ void vdPrint(char *characters,uint32_t hexColor){
 }
 
 void vdDeleteChar(){
-	vdPrintRect(cursor.posX/bytesPerPixel,cursor.posY/pitch,getCurrentFont().size.realWidth,getCurrentFont().size.height,0x00000000);
+	vdPrintRect(cursor.posX/bytesPerPixel,cursor.posY/pitch,getCurrentFont().size.realWidth,getCurrentFont().size.height,BLACK);
 	vdUpdateCursor(-1,0);
-	vdPrintRect(cursor.posX/bytesPerPixel,cursor.posY/pitch,getCurrentFont().size.realWidth,getCurrentFont().size.height,0x00000000);
+	vdPrintRect(cursor.posX/bytesPerPixel,cursor.posY/pitch,getCurrentFont().size.realWidth,getCurrentFont().size.height,BLACK);
 	vdPrintCursor();
 }
-
 
 void vdResize(){
 	vdClearScreen();
 	scrollUpdateBuffer = 0;
-	int i,j = 0;
 	char c;
-	/* int z,r,q = 0;
-	
-	char b;
-	int limit = (widthScreen/getCurrentFont().size.realWidth) * (heightScreen/getCurrentFont().size.height); */
-	/*while(z < index){
-		b = charsInScreen[z++];
-		if(b == '\n'){
-			q += (((widthScreen*bytesPerPixel - cursor.posX) / bytesPerPixel) / getCurrentFont().size.realWidth);
-		}
-		else
-			q++;
-
-		
-	}
-	*/
-	for(i = 0, j=0;j<index;i++,j++){
+	for(int j=0;j<index;j++){
 		c = charsInScreen[j];
-		//if(cursor.posY != heightScreen * pitch && cursor.posX != widthScreen* bytesPerPixel){
 		if(c == '\n'){
-			i+= (((widthScreen*bytesPerPixel - cursor.posX) / bytesPerPixel) / getCurrentFont().size.realWidth);
 			vdNewLine();
 		}
 		else{
 			fgColor = colorsInScreen[j];	
 			vdPrintChar(c);
-			}
-
-		//}		
+			}	
 	}
 	scrollUpdateBuffer = 1;
 }
@@ -231,11 +210,9 @@ void vdClearScreen(){
 
 void vdClearBuffer(){
 	memset(charsInScreen,' ',MAXCHARSINSCREEN);
-	memset(colorsInScreen,0,4*maxCharsInScreen);
+	memset(colorsInScreen,0,4*MAXCHARSINSCREEN);
 	index = 0;
 }
-
-
 
 void updateCharsInScreen(int lines){
 	for(int i=lines * (widthScreen/getCurrentFont().size.realWidth),j=0;i<MAXCHARSINSCREEN;i++){
