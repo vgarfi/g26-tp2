4457c8bd9f1466000fe19683392562d3813fd56b
NickArancibia <narancibiacarabajal@gmail.com>
2024-06-01 12:22:25 -0300

    remove unnecessary stackframe

--- Kernel/asm/speaker.asm
+++ Kernel/asm/speaker.asm
@@ -3,25 +3,13 @@ GLOBAL inb
 
 section .text
 inb:
-    push rbp
-    mov rbp, rsp
-
     mov rdx, rdi
     mov rax, 0
     in al, dx
-
-    mov rsp, rbp
-    pop rbp
     ret
 
 outb:
-    push rbp
-    mov rbp, rsp
-
     mov rax, rsi
     mov rdx, rdi
     out dx, al
-    
-    mov rsp, rbp
-    pop rbp
     ret
