ba4e901ad3fe42b9ebbb430a9fd056a38f0df149
Bordita <bordatomas1@gmail.com>
2024-06-04 18:21:07 -0300

    spanish comments to english comment

--- Kernel/idtLoader.c
+++ Kernel/idtLoader.c
@@ -3,10 +3,10 @@
 #include <defs.h>
 #include <interrupts.h>
 
-#pragma pack(push)		/* Push de la alineación actual */
-#pragma pack (1) 		/* Alinear las siguiente estructuras a 1 byte */
+#pragma pack(push)		 /* Push the current alignment */
+#pragma pack (1) 		/* Align the following structures to 1 byte */
 
-/* Descriptor de interrupcion */
+/* Interrupt Descriptor */
 typedef struct {
   uint16_t offset_l, selector;
   uint8_t cero, access;
@@ -14,9 +14,9 @@ typedef struct {
   uint32_t offset_h, other_cero;
 } DESCR_INT;
 
-#pragma pack(pop)		/* Reestablece la alineación actual */
+#pragma pack(pop)		/* Restore the previous alignment */
 
-DESCR_INT * idt = (DESCR_INT *) 0;	// IDT de 255 entradas
+DESCR_INT * idt = (DESCR_INT *) 0;	// IDT with 255 entries
 
 static void setup_IDT_entry (int index, uint64_t offset);
 
--- Kernel/include/defs.h
+++ Kernel/include/defs.h
@@ -6,12 +6,12 @@
 #define _defs_
 
 
-/* Flags para derechos de acceso de los segmentos */
-#define ACS_PRESENT     0x80            /* segmento presente en memoria */
-#define ACS_CSEG        0x18            /* segmento de codigo */
-#define ACS_DSEG        0x10            /* segmento de datos */
-#define ACS_READ        0x02            /* segmento de lectura */
-#define ACS_WRITE       0x02            /* segmento de escritura */
+/* Flags for segment access rights */
+#define ACS_PRESENT     0x80            /* segment present in memory */
+#define ACS_CSEG        0x18            /* code segment */
+#define ACS_DSEG        0x10            /* data segment */
+#define ACS_READ        0x02            /* read segment */
+#define ACS_WRITE       0x02            /* write segment */
 #define ACS_IDT         ACS_DSEG
 #define ACS_INT_386 	0x0E		/* Interrupt GATE 32 bits */
 #define ACS_INT         ( ACS_PRESENT | ACS_INT_386 )
