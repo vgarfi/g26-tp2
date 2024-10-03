7ed45ee8aa5392a23fccbc5e2d8dfeca7ce1f144
Nicole <nsalama@itba.edu.ar>
2024-10-03 17:36:33 -0300

    feat: added files to gitignore

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-if [["$1" = "gdb" ]]; then
+if [[ "$1" = "gdb" ]]; then
 	qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -d int
 else
 	qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
 fi
+
+#!/bin/bash
+#if [[ "$1" = "gdb" ]]; then
+#  qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512
+#else
+#  qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
+#fi
