8e79f0e48b3312454e94a2499d0dc785b1373fca
Nicole <nsalama@itba.edu.ar>
2024-10-03 11:42:37 -0300

    feat: run updated to run with gdb if wanted

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
+if [["$1" = "gdb" ]]; then
+	qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -d int
+else
+	qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
+fi
